964792a89ece37685b904f2918daf6297eb14b38
Shantanu Roy <Mr.Shantanu.Roy@outlook.com>
2024-06-05 16:34:58 +0200

    mqtt folder

--- tests/test_mqtt_dynamodb_stack.py
+++ tests/test_mqtt_dynamodb_stack.py
@@ -1,10 +1,10 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from mqtt_dynamodb_stack import MqttDynamodbStack
+from mqtt.mqtt_dynamodb_stack import MqttDynamoDBStack
 
 def test_sqs_queue_created():
     app = core.App()
-    stack = MqttDynamodbStack(app, "MqttDynamodbStack")
+    stack = MqttDynamoDBStack(app, "MqttDynamoDBStack")
     template = assertions.Template.from_stack(stack)
 
